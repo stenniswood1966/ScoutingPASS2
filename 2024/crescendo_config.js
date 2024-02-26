a551009ca4d110518477722c3692e09a25d7e745
Steven Tenniswood <steven.tenniswood@gmail.com>
2024-02-26 08:44:55 -0500

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2024miket",
+      "defaultValue": "2024mimil",
       "required": "true"
     },
     { "name": "Match Level",
